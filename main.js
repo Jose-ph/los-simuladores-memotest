2259005b8031b01ff1075df76198f08896bf7db9
Jose <jose_esteban_basualdo@hotmail.com>
2021-02-04 17:30:18 -0300

    agrega funciones y fin de juego

--- main.js
+++ main.js
@@ -1,100 +1,86 @@
 
 const cards = document.querySelectorAll('.memory-card');
 
-let hasflip = false;
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
-let firstCard;
-let secondCard;
+let wonCards = [];
 
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
+  this.classList.add('flip');
 
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
 
+    return;
+  }
 
-
-
-
-
-
-
-
-cards.forEach(card => card.addEventListener("click" , flipCard));
-
-
-
-function flipCard(){  // this es el elemento que genera el evento (?) algo asi
-
-
-    //this.classList.toggle('flip'); Esto lo convierte en add
-
-    this.classList.add('flip');
-
-
-    if(!hasflip){  //primer click
-
-        firstCard = this ;
-
-        hasflip = true;
-
-       // console.log( hasflip , firstCard);
-   
-    } else{
-
-        hasflip = false;
-        secondCard = this;
-
-
-        //console.log( firstCard , secondCard);    
-
-        //console.log(firstCard.dataset.character);
-        //console.log(secondCard.dataset.character);
-
-        if(firstCard.dataset.character === secondCard.dataset.character){
-
-            firstCard.removeEventListener('click',flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else{
-
-            setTimeout(()=>{
-
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-        }
-            ,500)
-
-
-
- 
-    }
-  
-
-   
-    
-    //console.log("me tocaron");
-
-
-    
-
+  secondCard = this;
+  checkForMatch();
 }
 
+function checkForMatch() {
+  let isMatch = firstCard.dataset.character === secondCard.dataset.character;
 
+  isMatch ? disableCards() : unflipCards();
 
+  if(isMatch){
 
+    wonCards.push(firstCard);
+  console.log(wonCards);
 
+  }
 
+  if(wonCards.length === 6){
 
+    setTimeout(()=>{
 
+        alert("Ganaste !");
 
+    },500)
+        
+  }
+  
+}
 
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
+  resetBoard();
+}
 
+function unflipCards() {
+  lockBoard = true;
 
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
 
+    resetBoard();
+  }, 800);
+}
 
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
 
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
+cards.forEach(card => card.addEventListener('click', flipCard));
 
-
-
+ 
 
 
 
@@ -311,4 +297,4 @@ function flipCard(){  // this es el elemento que genera el evento (?) algo asi
 
 // });
 
-}
+
